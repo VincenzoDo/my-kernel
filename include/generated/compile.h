969335653bb0f0d51e5170379d89835b8660059a
Vincenzo Do <vincenzo.do@master.hes-so.ch>
2015-11-05 15:37:04 +0100

    second commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 SMP PREEMPT Wed Nov 4 14:11:39 CET 2015"
+#define LINUX_COMPILE_BY "vince"
+#define LINUX_COMPILE_HOST "vincenzo-ub"
+#define LINUX_COMPILER "gcc version 4.9.3 20150113 (prerelease) (Linaro GCC 4.9-2015.01-3) "
